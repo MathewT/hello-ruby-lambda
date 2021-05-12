0608fbceade16b886ea75a80cd5d030b003818ca
Mathew Thomas <mathew.thomas@longdivision.com>
2021-05-11 18:55:02 -0700

    fixed interpolation

--- src/lambda_function.rb
+++ src/lambda_function.rb
@@ -10,14 +10,17 @@ def lambda_handler(event:, context:)
   logger.info('## ENVIRONMENT VARIABLES')
   vars = Hash.new
   ENV.each do |variable|
+    logger.info("Type is #{variable.class} and length is #{variable.length()}")
     vars[variable[0]] = variable[1]
   end
   logger.info(vars.to_json)
+  logger.info('## ENV.to_a')
+  # logger.info(ENV.to_a)
   logger.info('## EVENT')
   logger.info(event.to_json)
+  logger.info(event)
   logger.info('## CONTEXT')
   # logger.info(context)
-  logger.info("\n")
   logger.info(context.function_name)
   { statusCode: 200, body: JSON.generate('hello, Jeff Goldblum!') }
 end
