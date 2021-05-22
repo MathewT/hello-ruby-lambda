d4ca47c1af67a0565fd413ee4e777bb401cb48b0
Mathew Thomas <mathew.thomas@longdivision.com>
2021-05-22 12:05:36 -0700

    moved logging of ENV, event and context objects to EnvInfo object

--- src/lambda_function.rb
+++ src/lambda_function.rb
@@ -9,23 +9,10 @@ require 'aws-xray-sdk/lambda'
 def lambda_handler(event:, context:)
   logger = Logger.new($stdout)
   logger.info("helo ruby lambda!")
-  logger.info('## ENVIRONMENT VARIABLES')
-  vars = Hash.new
-  ENV.each do |variable|
-    logger.info("Type is #{variable.class} and length is #{variable.length()}")
-    vars[variable[0]] = variable[1]
-  end
-  logger.info(vars.to_json)
-  logger.info('## ENV.to_a')
-  # logger.info(ENV.to_a)
-  logger.info('## EVENT')
-  logger.info(event.to_json)
-  logger.info(event)
-  logger.info('## CONTEXT')
-  # logger.info(context)
-  logger.info(context.function_name)
 
   env_info = EnvInfo.new
+  env_info.log_env
+  env_info.log_event_and_context(event, context)
   
   { statusCode: 200, body: JSON.generate('hello, Jeff Goldblum!') }
 end
--- src/lib/env_info.rb
+++ src/lib/env_info.rb
@@ -3,6 +3,28 @@ require 'logger'
 class EnvInfo
   def initialize
     @logger = Logger.new($stdout)
-    @logger.info("hello env info class")
+    @logger.info("initialize env info class")
+  end
+
+  def log_env
+    vars = Hash.new
+    ENV.each do |variable|
+      # @logger.info("Type is #{variable.class} and length is #{variable.length()}")
+      vars[variable[0]] = variable[1]
+    end
+    @logger.info("## ENV vars:")
+    @logger.info(vars.to_json)
+    # @logger.info('## ENV.to_a')
+    # logger.info(ENV.to_a)
+  end
+
+  def log_event_and_context(event, context)
+    @logger.info('## ENVIRONMENT VARIABLES')
+    @logger.info('## EVENT')
+    @logger.info(event.to_json)
+    @logger.info(event)
+    @logger.info('## CONTEXT')
+    @logger.info(context)
+    @logger.info(context.function_name)
   end
 end
